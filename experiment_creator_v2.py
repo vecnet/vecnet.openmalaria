fbab6bb1014243b320fc94fafc15bc7a6f8bb41c
Alex Vyushkov <baguage@gmail.com>
2014-08-06 21:31:18 -0400

    Added copyright information

--- experiment_creator_v2.py
+++ experiment_creator_v2.py
@@ -1,6 +1,13 @@
-#!/bin/env python2
-# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
-# the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# -*- coding: utf-8 -*-
+#
+# This file is part of the vecnet.openmalaria package.
+# For copyright and licensing information about this package, see the
+# NOTICE.txt and LICENSE.txt files in its top-level directory; they are
+# available at https://github.com/vecnet/vecnet.openmalaria
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License (MPL), version 2.0.  If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import json
 import itertools
