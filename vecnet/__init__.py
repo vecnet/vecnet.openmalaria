256cacfca4098bc9d903d50f7109055d831fddb9
Alex Vyushkov <baguage@gmail.com>
2014-08-14 23:15:44 -0400

    Maked vecnet.openmalaria package

--- vecnet/__init__.py
+++ vecnet/__init__.py
@@ -7,7 +7,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License (MPL), version 2.0.  If a copy of the MPL was not distributed
 # with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
 
 # This is required to implement namespaces in python 2.x
 # http://legacy.python.org/dev/peps/pep-0420/#namespace-packages-today
