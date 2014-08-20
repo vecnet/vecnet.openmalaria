27d0d9688397df55789a947f9773966db72f46e0
Alex Vyushkov <baguage@gmail.com>
2014-08-20 08:35:21 -0400

    Support for running experiment creator on the command line (Windows)

---
+++ expand.cmd
@@ -0,0 +1,1 @@
+python -m vecnet.openmalaria.bin.expand %1
--- vecnet/openmalaria/__init__.py
+++ vecnet/openmalaria/__init__.py
@@ -8,3 +8,4 @@
 # License (MPL), version 2.0.  If a copy of the MPL was not distributed
 # with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
+from .experiment_creator_v2 import ExperimentDescription
