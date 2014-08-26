18dc2c0ff4d24f2341bd007447bca88140459473
Alex Vyushkov <baguage@gmail.com>
2014-08-26 17:35:01 -0400

    Fixed wrong classifier in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: Mozilla Public License",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
