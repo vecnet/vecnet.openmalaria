46e3dba78178e50fec08efe5c1f10ed18d67913a
Alex Vyushkov <alexander.vyushkov[at]nd.edu>
2014-12-21 21:39:36 -0500

    v0.3.0

--- vecnet/openmalaria/tests/output_parser_tests.py
+++ vecnet/openmalaria/tests/output_parser_tests.py
@@ -22,19 +22,21 @@ class TestOutputParser(unittest.TestCase):
         self.assertEqual(output_parser.survey_time_list, [730, 803, 876, 949, 1022, 1095])
         self.assertEqual(output_parser.get_cts_measures(), ["simulated EIR"])
         self.assertEqual(len(output_parser.cts_output_data["simulated EIR"]), 1096)
-        self.assertEqual(set(output_parser.get_survey_measures()), ({[(3, 2), (3, 1), (14, 1), (14, 2)]}))
+        self.assertEqual(set(output_parser.get_survey_measures()), ({(3, 2), (3, 1), (14, 1), (14, 2)}))
         measure_names = set()
         for measure in output_parser.get_survey_measures():
             measure_names.add(output_parser.get_survey_measure_name(measure_id=measure[0],
                                                                     third_dimension=measure[1]))
         self.assertEqual(measure_names,
-                         ({["nPatent(18 - 90)", "nPatent(0 - 18)", "nUncomp(0 - 18)", "nUncomp(18 - 90)"]}))
+                         ({"nPatent(18 - 90)", "nPatent(0 - 18)", "nUncomp(0 - 18)", "nUncomp(18 - 90)"}))
         self.assertEqual(output_parser.survey_output_data,
                          {(3, 2): [[730, 16.0], [803, 17.0], [876, 16.0], [949, 14.0], [1022, 12.0], [1095, 10.0]],
                           (3, 1): [[730, 27.0], [803, 16.0], [876, 15.0], [949, 23.0], [1022, 20.0], [1095, 13.0]],
                           (14, 1): [[730, 1116.0], [803, 99.0], [876, 82.0], [949, 100.0], [1022, 102.0], [1095, 92.0]],
                           (14, 2): [[730, 280.0], [803, 21.0], [876, 26.0], [949, 28.0], [1022, 13.0], [1095, 24.0]]}
                          )
+        self.assertEqual(output_parser.survey_output_data[(3, 2)],
+                         [[730, 16.0], [803, 17.0], [876, 16.0], [949, 14.0], [1022, 12.0], [1095, 10.0]])
 
     def setUp(self):
         pass
