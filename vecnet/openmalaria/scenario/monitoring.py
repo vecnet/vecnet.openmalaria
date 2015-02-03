8b2f7beb92877a6087e1c22a58aade0dffb39d20
Nicolas Reed <Nicolas.Reed.102@nd.edu>
2015-02-03 10:52:01 -0500

    -Remove: Now unnecessary import.

--- vecnet/openmalaria/scenario/monitoring.py
+++ vecnet/openmalaria/scenario/monitoring.py
@@ -8,7 +8,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License (MPL), version 2.0.  If a copy of the MPL was not distributed
 # with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from xml.etree import ElementTree
 from xml.etree.ElementTree import Element
 from vecnet.openmalaria.scenario.core import attribute, Section, section
 
