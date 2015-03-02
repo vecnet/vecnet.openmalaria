fa1340e68898f19e03a0d9350fd2e7ed40df804a
Alex Vyushkov <alexander.vyushkov[at]nd.edu>
2015-03-02 15:51:48 -0500

    v0.8.0 - add scenario.interventions.vectorPop section

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vecnet.openmalaria",
-    version="0.7.1",
+    version="0.8.0",
     author="Alex Vyushkov, Diggory Hardy",
     author_email="vecnet@vecnet.org",
     description="Openmalaria library for VecNet-CI project",
