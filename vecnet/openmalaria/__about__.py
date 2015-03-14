87a2f4b12ee3425a452b653630a2595dd7b7a54a
Jimm Domingo <jdomingo@green-code.com>
2015-03-14 16:27:25 -0400

    Added a new module to the package -- it serves as the single source for the project's metadata.  Revised the setup.py and conf.py code to get the project's version from this new file.

---
+++ vecnet/openmalaria/__about__.py
@@ -0,0 +1,9 @@
+# Single source of metadata about the project that's used by setup.py and
+# docs/conf.py
+
+# Some segments of public version identifer (PEP 440)
+VERSION_RELEASE = "0.8.2"
+VERSION_PRE_RELEASE = ""    # e.g., "a4", "b1", "rc3" or "" (final release)
+VERSION_POST_RELEASE = ""   # e.g., ".post1"
+
+VERSION = VERSION_RELEASE + VERSION_PRE_RELEASE + VERSION_POST_RELEASE
