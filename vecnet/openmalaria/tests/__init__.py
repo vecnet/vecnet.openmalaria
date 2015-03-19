220aff6d67f0feb04b9670087fac1ca2b4c81620
Nicolas Reed <Nicolas.Reed.102@nd.edu>
2015-03-19 14:58:41 -0400

    Remove: import of deprecated package.

--- vecnet/openmalaria/tests/__init__.py
+++ vecnet/openmalaria/tests/__init__.py
@@ -2,7 +2,6 @@ from .test_experiment import *
 from .test_output_parser import *
 from .test_scenario import *
 from .test_get_schema_version import *
-from .test_xmlinputfile import *
 
 if __name__ == "__main__":
     unittest.main()
