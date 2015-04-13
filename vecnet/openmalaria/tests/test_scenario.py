357741a399e8fce87451f7ca01163ef871d0319f
Nicolas Reed <Nicolas.Reed.102@nd.edu>
2015-04-13 14:00:43 -0400

    Add: TreatmentAction and Deploy support.

--- vecnet/openmalaria/tests/test_scenario.py
+++ vecnet/openmalaria/tests/test_scenario.py
@@ -89,11 +89,26 @@ class TestScenario(unittest.TestCase):
                 self.assertEqual(drug.initialACR, 0.96)
                 self.assertEqual(drug.compliance, 0.892)
                 self.assertEqual(drug.nonCompliersEffective, 0.8544)
+                self.assertEqual(drug.treatmentAction.name, 'clear blood-stage infections')
+                self.assertEqual(drug.treatmentAction.timesteps, 1)
+                self.assertEqual(drug.treatmentAction.stage, 'blood')
 
         scenario.healthSystem.ImmediateOutcomes.drugs.add("CQ", 0.5, ["initialACR", "compliance"])
         self.assertEqual(len(scenario.healthSystem.ImmediateOutcomes.drugs), 4)
-        self.assertEqual(scenario.healthSystem.ImmediateOutcomes.drugs["CQ"].initialACR, 0.5)
-        self.assertEqual(scenario.healthSystem.ImmediateOutcomes.drugs["CQ"].compliance, 0.5)
+
+        drug = scenario.healthSystem.ImmediateOutcomes.drugs["CQ"]
+        self.assertEqual(drug.initialACR, 0.5)
+        self.assertEqual(drug.compliance, 0.5)
+        self.assertEqual(drug.treatmentAction, None)
+
+        drug.treatmentAction = 'test'
+        drug.treatmentAction.timesteps = 1
+        drug.treatmentAction.stage = 'blood'
+
+        self.assertEqual(drug.treatmentAction.name, 'test')
+        self.assertEqual(drug.treatmentAction.timesteps, 1)
+        self.assertEqual(drug.treatmentAction.stage, 'blood')
+
 
     def test_entomology(self):
         scenario = Scenario(open(os.path.join(base_dir, os.path.join("input", "scenario70k60c.xml"))).read())
