6cec5230937118fae9d3e5e1345c3075f22301d8
Nicolas Reed <Nicolas.Reed.102@nd.edu>
2015-07-31 14:32:52 -0400

    Change: Update to v0.9.1.

--- vecnet/openmalaria/__about__.py
+++ vecnet/openmalaria/__about__.py
@@ -3,7 +3,7 @@
 
 # Some segments of public version identifer (PEP 440)
 VERSION_RELEASE = "0.9.1"
-VERSION_PRE_RELEASE = ".dev5"    # e.g., "a4", "b1", "rc3" or "" (final release)
+VERSION_PRE_RELEASE = ""    # e.g., "a4", "b1", "rc3" or "" (final release)
 VERSION_POST_RELEASE = ""   # e.g., ".post1"
 
 VERSION = VERSION_RELEASE + VERSION_PRE_RELEASE + VERSION_POST_RELEASE
